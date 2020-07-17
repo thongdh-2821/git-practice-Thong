947752669c972345562207073c409e7ff0fb0b07
ngocvt-0484 <vu.thi.ngoc@sun-asterisk.com>
2020-07-17 14:42:18 +0700

    Create user

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,7 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build micropost_params
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "demo git day nha"
       redirect_to root_url
     else
       @feed_items = []
